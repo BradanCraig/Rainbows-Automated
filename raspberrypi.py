ecf4c13c89843ffcc7c5fa422dc4d9d3eda7c0f9
Bradan Craig <bradancraig2004@gmail.com>
2025-04-21 13:50:57 -0400

    Working image generation

--- raspberrypi.py
+++ raspberrypi.py
@@ -7,10 +7,17 @@ import subprocess
 
 #this stuff is also hard coded in. For deployment a database will have to be implimented as well as a set up wizzard for all of the raspberry pi stuff.
 
-def run_script(script_name="/home/lizawhite/Desktop/Code_for_Camera_Capture.py"):
+def run_script(script_name="/home/lizawhite/Desktop/SetupCode.py"):
     print("ran")
     result_message = subprocess.run(f"ssh lizawhite@10.4.119.62 python3 {script_name}")
     print(result_message)
+    if result_message.returncode == 0:
+        local_path = "C:/HowelLab/Rainbows/Automated_Rainbows/Rainbows-Automated/images"
+        scp_command = f'scp lizawhite@10.4.119.62:/home/lizawhite/image.jpg "{local_path}"'
+        pulling_img_message = subprocess.run(scp_command, shell=True)
+        print(pulling_img_message)
+    else:
+        raise Exception("Command failed executing script")
     return 0
 
     #This now runs the script on the Raspberry Pi, need the camera re connected and to re configure the script to take input parameters
