f71d0c6248367be9edf92f9b043bccf35b33aaa7
Bradan Craig <bradancraig2004@gmail.com>
2025-05-07 15:23:25 -0400

    New Stop button

--- server.py
+++ server.py
@@ -4,11 +4,14 @@ import requests
 import base64
 from helpers import *
 import time
+import threading
 
 
 app=Flask(__name__)
 
-
+#Globals
+thread=None
+stop_pictures = threading.Event()
 
 @app.route('/')
 def home():
@@ -31,25 +34,29 @@ def run():
 
 
 @app.route('/send_dur_and_freq', methods=['POST'])
-def sending_data():
-    data = request.get_json()
-    duration = data.get("duration")
-    frequency = data.get("frequency")
+def sending_data(stoping, duration, frequency):
 
 
     
-    url = 'http://10.4.119.62:8000/get_images'
+    url = 'http://10.4.119.62:5000/get_images'
     data = {'duration': duration, 'frequency': frequency}
     headers = {'Content-Type': 'application/json'}
 
     try:
         number_of_images = (duration*frequency)
         for i in range(number_of_images+1):
+
+            if stoping.is_set():
+                return "Stopped Early"
             
+            #Turn On LED
+
             response = requests.post(url, json=data, headers=headers)
             image = decode_image(encoded_img=response.json()['image'])
             image.save(f"images/image_{i}.png")
             
+            #turn off LED
+
             if i != number_of_images:
                 time.sleep((1/frequency)*60)
         
@@ -60,6 +67,26 @@ def sending_data():
         print(e)
         return f"Error sending data: {e}", 500
 
+@app.route("/stop")
+def stop():
+    global stop_pictures
+    stop_pictures.set()
+    return "Stopped"
+
+@app.route("/start", methods=["POST"])
+def start():
+        
+    data = request.get_json()
+    duration = data.get("duration")
+    frequency = data.get("frequency")
+
+    global thread, stop_pictures
+    if thread is None or not thread.is_alive():
+        stop_pictures.clear()
+        thread = threading.Thread(target=sending_data, args=(stop_pictures, duration, frequency))
+        thread.start()
+        
+    return "started"
 
 @app.route("/setup")
 def setup():
